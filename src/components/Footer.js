44904c66070f338f04a4fac579ea2118092bd9ff
Elyes T. <139150024+CodeSailor411@users.noreply.github.com>
2025-01-15 05:49:21 +0000

    changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Elyes Thabet</h3>
+          <h3>Developed by Elyes Thabet</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} Elyes Thabet</h3>
